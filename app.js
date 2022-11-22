cfa1bd524c392ab6d2fe6a5de5a391f3c32dcd2c
Temirov Erali <eralitemirov129@gmail.com>
2022-11-22 10:43:12 +0500

    q

---
+++ app.js
@@ -0,0 +1,25 @@
+alert("Bizning fermamizga xush kelibsiz");
+alert("Saytimizdan foydalanishingiz uchun shaxsiy malumotlaringizni kiriting");
+var ismingiz = prompt("Ismingizni kiriting? ");
+console.log("Ismi:" + ismingiz);
+var familyangiz = prompt("Familyangizni kiriting? ");
+console.log("Familya:" + familyangiz);
+var yoshingiz = prompt("Yoshingizni kiriting? ");
+console.log("Yosh:" + yoshingiz);
+var manzil = prompt("Yashash joyingizni kiriting? ");
+console.log("Manzil:" + manzil);
+var raqam = prompt("Passport raqamingiznikiriting ");
+console.log("Passport:" + raqam);
+prompt("Qayerga bormoqchisiz");
+let som = +prompt("Mablag'izni kiriting");
+pulingiz = 1260 * 11170;
+if ((som) => pulingiz) {
+  alert("Sizga yoqimli xordiq tilaymiz");
+} else if (som < pulingiz) {
+  alert(
+    Math.floor(Math.random() * 31) +
+      "  kundan so'ng qayta saytimizdan foydalanishingiz mumkin "
+  );
+} else {
+  alert("Iltimos mablag'ingizni raqam shaklida kiriting");
+}
